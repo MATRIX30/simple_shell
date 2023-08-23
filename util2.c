4f96db290e777bb1113f84ba82db0889e31ef36f
Matrix30 <Matrixboy30@gmail.com>
2023-08-24 00:23:01 +0100

    added _getline function

--- util2.c
+++ util2.c
@@ -1,4 +1,7 @@
 #include "main.h"
+int _atoi(char *s);
+ssize_t _getline(char **lineptr, size_t *n, int fd);
+char *_strtok(char *str, const char *del);
 /**
 * _atoi - converts string to integer
 * @s: string of characters
@@ -73,3 +76,70 @@ char *_strtok(char *str, const char *del)
 	}
 	return (start);
 }
+
+/**
+* _getline - function to take data from stream and store in buffer
+* @lineptr: pointer to the buffer where data will be stored
+* @n: number of bytes to be written
+* @fd: file descriptor to the stream
+* Return: number of bytes writteni -1 if fail
+*/
+ssize_t _getline(char **lineptr, size_t *n, int fd)
+{
+	static char buffer[BUFFER_SIZE];
+	static size_t ind;
+	static size_t len;
+	size_t i = 0;
+	char *stream;
+
+	ind = 0;
+	len = 0;
+	if (lineptr == NULL || n == NULL)
+	{
+		return (-1);
+	}
+
+	if (*lineptr == NULL)
+	{
+		*n = BUFFER_SIZE;
+		*lineptr = malloc(*n);
+		if (*lineptr == NULL)
+		{
+			return (-1);
+		}
+	}
+
+	stream = *lineptr;
+	while (1)
+	{
+		if (ind >= len)
+		{
+			ind = 0;
+			len = read(fd, buffer, BUFFER_SIZE);
+			if (len <= 0)
+			{
+				return (len);
+			}
+		}
+		while (index < len && i < *n - 1 && buffer[ind] != '\n')
+		{
+			stream[i++] = buffer[ind++];
+		}
+		if (ind < len && buffer[ind] == '\n')
+		{
+			stream[i++] = buffer[ind++];
+			stream[i] = '\0';
+			return (i);
+		}
+
+		if (i >= *n - 1)
+		{
+			*n *= 2;
+			stream = realloc(stream, *n);
+			if (stream == NULL)
+				return (-1);
+			*lineptr = stream;
+		}
+	}
+}
+
